9e5c503d9a5b417bc25c968a6454f8bbd183b358
Sivabhargav123 <gsiva52892@gmail.com>
2026-04-03 14:13:51 +0530

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 import "./Hero.css";
-import profile from '../assets/IMG_20220925_184409_424.jpg'
+import profile from '../assets/IMG_20220925_174241.jpg'
 
 const Hero = () => {
   return (
